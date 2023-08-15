febffc0665726a2b16d388468dbecd3154e4e334
mutannejs <murillojustinoklein@gmail.com>
2023-08-15 15:47:00 -0300

    pacman atualizado

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pacman_threejs
+
+## Primeira modelagem do pacman
+
+![pacman.gif](assets/pacman.gif)
--- pacman.js
+++ pacman.js
@@ -1,14 +1,16 @@
 var scene;
 var camera;
 var renderer;
-var base;
-var base2;
+var pacman;
 var fase;
 var abrir;
 
+// Animação do pacman
 var render = function() {
+
 	requestAnimationFrame( render );
 
+	// seta se o pacman deve abrir ou fechar a boca, muda a cada 39 fases.
 	if (fase >= 39) {
 		if (abrir == 1) {
 			abrir = 0;
@@ -19,72 +21,152 @@ var render = function() {
 	} else {
 		fase++;
 	}
-	this.animateBase();
-	this.animateBase2();
+
+	// anima as duas partes do pacman
+	this.animatePacman();
 
 	renderer.render( scene, camera );
+
 };
 
-// cria a base do pacman
-var createABase = function() {
-	// argumentos : escala, linhas horizontais, linhas verticais, "o quanto da esfera desenhar, com Math.PI desenha só metade"
+// cria o pacman
+var createPacman = function() {
+
+	pacman = new THREE.Group();
+
+	// cria a metade de uma esfera e rotaciona ela em 90º, de modo que
+	//     ela fique em baixo
+	var me1 = createAMidEsphere();
+	me1.rotation.x += Math.PI*0.5;
+
+	// cria um disco e rotaciona ele em 90º, de modo que ele preencha a
+	//     meia esfera de baixo
+	var d1 = createADisc();
+	d1.rotation.x += Math.PI*0.5;
+
+	// cria a metade de uma esfera e rotaciona ela em 90º, de modo que
+	//     ela fique em cima
+	var me2 = createAMidEsphere();
+	me2.rotation.x -= Math.PI*0.5;
+
+	// cria um disco e rotaciona ele em 90º, de modo que ele preencha a
+	//     meia esfera de cima
+	var d2 = createADisc();
+	d2.rotation.x -= Math.PI*0.5;
+
+	pacman.add(me1);
+	pacman.add(me2);
+	pacman.add(d1);
+	pacman.add(d2);
+
+	scene.add( pacman );
+
+}
+
+// cria uma metade de esfera
+var createAMidEsphere = function() {
+
+	// material que será usado
+	var material = new THREE.MeshLambertMaterial( { color: 'yellow' } );
+
+	// cria a metade de uma esfera
+	//    argumentos : escala, linhas horizontais, linhas verticais, ângulo
+	//    para iniciar a figura, "o quanto da esfera desenhar, Math.PI
+	//    desenha só metade"
 	var geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI );
-	var material = new THREE.MeshBasicMaterial( { color: 'yellow' } );
-	base = new THREE.Mesh( geometry, material );
-	// rotaciona a metade da esfera 45º em torno do eixo x
-	base.rotation.x += Math.PI*0.5;
-	scene.add( base );
+	var meia_esfera = new THREE.Mesh( geometry, material );
+
+	// adiciona efeitos de sombras
+	meia_esfera.castShadow = true;
+	meia_esfera.receiveShadow = true;
+
+	return meia_esfera;
+
 };
 
-var createASecondBase = function() {
-	var geometry = new THREE.SphereGeometry( 1, 32, 16, 0, Math.PI );
-	var material = new THREE.MeshBasicMaterial( { color: 'yellow' } );
-	base2 = new THREE.Mesh( geometry, material );
-	base2.rotation.x -= Math.PI*0.5;
-	scene.add( base2 );
-};
+// cria um disco
+var createADisc = function() {
 
-// cria a animação da base
-var animateBase = function() {
+	// material que será usado
+	var material = new THREE.MeshLambertMaterial( { color: 'yellow' } );
+
+	// cria um disco
+	//    argumentos : escala, linhas de contorno
+	var circle = new THREE.CircleGeometry( 1, 32 );
+	var disco = new THREE.Mesh( circle, material );
+
+	// adiciona efeitos de sombras
+	disco.castShadow = true;
+	disco.receiveShadow = true;
+
+	return disco;
+
+}
+
+// cria a animação do pacman
+var animatePacman = function() {
 	if (abrir == 1) {
-		base.rotation.y -= 0.02;// rotaciona em torno de y
+		pacman.children[0].rotation.y -= 0.02;
+		pacman.children[1].rotation.y -= 0.02;
+		pacman.children[2].rotation.y += 0.02;
+		pacman.children[3].rotation.y += 0.02;
 	} else {
-		base.rotation.y += 0.02;
+		pacman.children[0].rotation.y += 0.02;
+		pacman.children[1].rotation.y += 0.02;
+		pacman.children[2].rotation.y -= 0.02;
+		pacman.children[3].rotation.y -= 0.02;
 	}
 }
 
-var animateBase2 = function() {
-	if (abrir == 1) {
-		base2.rotation.y -= 0.02;
-	} else {
-		base2.rotation.y += 0.02;
-	}
-}
+var createDirectionalLight = function() {
+	var directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+	directionalLight.position.set(5, 5, 5);
+	directionalLight.name='directional';
+	directionalLight.castShadow = true;
+	scene.add(directionalLight);
+};
+
+var createAmbientLight = function() {
+	var ambientLight = new THREE.AmbientLight(0x888888);
+	ambientLight.name='ambient';
+	ambientLight.castShadow = true;
+	scene.add(ambientLight);
+};
 
 var init = function() {
 
+	// cria a cena
 	scene = new THREE.Scene();
 
-	camera = new THREE.OrthographicCamera(//usando essa câmera só para a aimagem não distorcer por causa da perspectiva
-		-window.innerWidth/512,
-		window.innerWidth/512,
-		window.innerHeight/512,
-		-window.innerHeight/512,
-		0,
-		100
+	// cria a câmera
+	camera = new THREE.PerspectiveCamera(
+		50,
+		window.innerWidth / window.innerHeight,
+		0.1,
+		1000
 	);
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.body.appendChild( renderer.domElement );
-
-	camera.position.z = 2;
+	// reposiciona a câmera
+	camera.position.x = 2;
+	camera.position.y = 1;
+	camera.position.z = 5;
 	camera.lookAt(scene.position);
 
+	// cria o renderizador
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	document.body.appendChild( renderer.domElement );
+
+	// adiciona o pacman e as fontes de luz à cena
+	this.createPacman();
+	this.createDirectionalLight();
+	this.createAmbientLight();
+
+	// inicializa variáveis usadas na animação
 	fase = -1;
 	abrir = 1;
-	this.createABase();
-	this.createASecondBase();
 
 	this.render();
 
